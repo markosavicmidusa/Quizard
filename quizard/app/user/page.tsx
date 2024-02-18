cb9e3daa5c5fdac06812f7bb633f254ed14debd6
markosavicmidusa <savic.marko90@gmail.com>
2024-02-18 16:55:57 +0100

    adding the basic structure of /user and connecting to clerk api

--- quizard/app/user/page.tsx
+++ quizard/app/user/page.tsx
@@ -1,7 +1,18 @@
-export default function User(){
-    return(
-        <div>
-            USER
-        </div>
-    )
+"use client";
+import { useUser } from "@clerk/nextjs";
+ 
+export default function Page() {
+ 
+    const { isLoaded, isSignedIn, user } = useUser();
+ 
+    if (!isLoaded || !isSignedIn) {
+      return null;
+    }
+   
+    return <div>Hello, {user.firstName} welcome to Clerk
+    <div>Full name, {user.fullName} welcome to Clerk</div>
+    <div>ID {user.id} welcome to Clerk</div>
+    <div>createdat  {user.createdAt?.toString()} welcome to Clerk</div>
+   <h1>here will go list of quizes with approved or waiting prop</h1>
+    </div>;
 }
