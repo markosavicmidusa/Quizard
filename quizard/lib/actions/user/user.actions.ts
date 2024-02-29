1f5c2c9133aefde03a44ecad45376d6aacdf5473
markosavicmidusa <savic.marko90@gmail.com>
2024-02-29 19:31:49 +0100

    surpressing the error

--- quizard/lib/actions/user/user.actions.ts
+++ quizard/lib/actions/user/user.actions.ts
@@ -6,7 +6,24 @@ import { connectToDB } from "@/lib/mongoose";
 export async function GetUserByClerkID(id: string): Promise<IUser | null> {
     try {
         const user: IUser | null = await UserModel.findOne({ clerkId: id });
+
+        // Map the quizzes to plain objects
+        let plainObject: IUser;
+        if(user){
+            plainObject = {
+                id: user._id.toString(), // Convert ObjectId to string
+                clerkId: user.clerkId,
+                name: user.name,
+                surname: user.surname,
+                sportsClicked: user.sportsClicked,
+                popularClicked: user.popularClicked,
+                funClicked: user.funClicked,
+                scienceClicked: user.scienceClicked,
+                otherClicked: user.otherClicked
+            } as IUser
+        }
         return user;
+
     } catch (error) {
         console.error("Error retrieving user by Clerk ID:", error);
         return null;
