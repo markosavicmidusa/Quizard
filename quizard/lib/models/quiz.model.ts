d96df3cabdc7ef15f4cea3d5f507b164df4af123
markosavicmidusa <savic.marko90@gmail.com>
2024-02-29 11:54:39 +0100

    changes in merging

--- quizard/lib/models/quiz.model.ts
+++ quizard/lib/models/quiz.model.ts
@@ -1,19 +1,13 @@
 import mongoose, { Document, Schema, Types, Model } from "mongoose";
 
-<<<<<<< HEAD
 // Define the interface for the Answers document
-export interface IAnswers {
-=======
-// Subdocument definition for answers
-interface Answer {
->>>>>>> f2e50af0eaf01d417261fd79ab5548ee1b583f4c
+export interface IAnswer {
     value: string;
     isCorrect: boolean;
 }
 
-<<<<<<< HEAD
 // Define the Answers schema
-const AnswersSchema = new mongoose.Schema<IAnswers>({
+const AnswerSchema = new mongoose.Schema<IAnswer>({
     value: String,
     isCorrect: Boolean
 });
@@ -21,33 +15,22 @@ const AnswersSchema = new mongoose.Schema<IAnswers>({
 // Define the interface for the Question document
 export interface IQuestion {
     question: string;
-    answers: IAnswers[];
+    answers: IAnswer[];
 }
 
 // Define the Question schema
 const QuestionSchema = new mongoose.Schema<IQuestion>({
     question: String,
-    answers: [AnswersSchema] // Embed the Answers schema
+    answers: [AnswerSchema] // Embed the Answers schema
 });
 
 // Define the interface for the Result document
 export interface IResult {
-=======
-// Subdocument definition for questions
-interface Question {
-    question: string;
-    answers: Answer[];
-}
-
-// Subdocument definition for results
-interface Result {
->>>>>>> f2e50af0eaf01d417261fd79ab5548ee1b583f4c
     from: number;
     to: number;
     result: string;
 }
 
-<<<<<<< HEAD
 // Define the Result schema
 const ResultSchema = new mongoose.Schema<IResult>({
     from: Number,
@@ -69,46 +52,10 @@ const QuizSchema = new mongoose.Schema<IQuiz>({
     active: Number
 });
 
-// Define the Quiz model using the schema
-const QuizModel = mongoose.models.Quiz || mongoose.model<IQuiz>('Quiz', QuizSchema);
-=======
-// Document definition for the quiz
-export interface IQuiz extends Document {
-    questions: Question[];
-    results: Result[];
-    active: number;
-}
-
-// Define the answer subdocument schema
-const answerSchema = new Schema<Answer>({
-    value: String,
-    isCorrect: Boolean
-}, { _id: false });
-
-// Define the question subdocument schema
-const questionSchema = new Schema<Question>({
-    question: String,
-    answers: [answerSchema] // Array of answer subdocuments
-}, { _id: false });
-
-// Define the result subdocument schema
-const resultSchema = new Schema<Result>({
-    from: Number,
-    to: Number,
-    result: String
-}, { _id: false });
-
-// Define the main quiz schema
-const QuizSchema = new Schema<IQuiz>({
-    questions: [questionSchema], // Array of question subdocuments
-    results: [resultSchema], // Array of result subdocuments
-    active: Number
-});
-
 // Define the type for the document properties
 type QuizDocumentProps = {
-    questions: Types.DocumentArray<Question>;
-    results: Types.DocumentArray<Result>;
+    questions: Types.DocumentArray<IQuestion>;
+    results: Types.DocumentArray<IResult>;
 };
 
 // Define the type for the model
@@ -116,7 +63,6 @@ type QuizModelType = Model<IQuiz, {}, QuizDocumentProps>;
 
 // Create the model
 const QuizModel = mongoose.models.Quiz || mongoose.model<IQuiz, QuizModelType>('Quiz', QuizSchema);
->>>>>>> f2e50af0eaf01d417261fd79ab5548ee1b583f4c
 
 // Export the Quiz model
-export default QuizModel ;
+export default QuizModel;
